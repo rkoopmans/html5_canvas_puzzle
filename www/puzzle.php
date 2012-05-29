a1cc54eacada7b5eeb150b558950aa0ed6a06bb2
Remco Koopmans <remkkoopmans@gmail.com>
2012-05-29 12:19:32 +0200

    Upleuding

---
+++ www/puzzle.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript Puzzle</title>
+        <script type="text/javascript" src="js/puzzle.js"></script>
+        <link rel="stylesheet" href="styles/main.css" />
+    </head>
+    <body>
+        <canvas width="600" height="600" id="cPuzzle">
+            Your browser does not support HTML5 canvas
+        </canvas>
+        <script type="text/javascript">
+            onDocumentReady();
+        </script>
+    </body>
+</html>
